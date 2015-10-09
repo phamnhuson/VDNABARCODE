43950d77fcb19ab6ad7eddf14d04bdef899f67c6
Trungtr1 <quangtrung.phl@gmail.com>
2015-10-09 17:17:54 +0700

    fix view inew

--- app/Http/Controllers/InewController.php
+++ app/Http/Controllers/InewController.php
@@ -34,7 +34,7 @@ class InewController extends Controller {
 			$viewData['new']=$new;
 		}
 		
-		$list_new = DB::table('news')->where('created_by',$user['id'])->paginate(20);
+		$list_new = DB::table('news')->where('created_by',$user['id'])->paginate(10);
 		
 		$viewData['list_new'] = $list_new;
 		
--- resources/views/inew.blade.php
+++ resources/views/inew.blade.php
@@ -25,6 +25,20 @@
 </div>
 <div class="box">
 	<div class="row">
+		<div class="col-md-12" style="padding:0px">
+			<div class="nav-tabs-custom" style="margin-bottom: 0px; box-shadow:none;">
+				<ul class="nav nav-tabs">
+					<li class='active'>
+						<a href="#tab1" data-toggle='tab' id='page1'>Cập nhật bài viết</a>
+					</li>
+					<li>
+						<a href="#tab2" data-toggle='tab' id='page2'>Danh sách bài viết</a>
+					</li>						
+				</ul>
+			</div>
+		</div>				
+	</div>
+	<div class="row">
 		<div class="col-lg-12" style="padding:0px;">
 			@if (Session::has('responseData'))
 				@if (Session::get('responseData')['statusCode'] == 1)
@@ -42,72 +56,83 @@
 						@endforeach
 					</ul>
 				</div>
-			@endif
+			@endif			
 			{!! Form::open(array('method' => (isset($data['new'])) ? 'PUT' : 'POST', 'enctype'=>'multipart/form-data', 'files' => true)) !!}
-				<table width="100%" class="form-table">
-					<tr>
-						<td class="col-lg-2" style="padding:0px;">
-							<div class="form-group" >
-								<div class="upanh" id="upanh_1" data_id="1">
-									<a> 
-										<img class='col-lg-12' style="padding:6px;border:2px dashed #0087F7;height:130px;width:140" id="img_1" src="@if (isset($data['new'])){{asset('public/uploads/img/new_'.$data['new'][0]['new_id'].'.jpg')}}@else{{asset('public/img/add.png')}}@endif" alt="Chọn mẫu vật" />										
-									</a>									
-								</div>
-								<div id="btx_1">
-								</div>								
-								{!! Form::file('images', array('class'=>'form-control imgInp','style'=>'display:none;','id'=>'imgInp_1','data_id'=>'1')) !!}
-							</div>				
-						</td>
-						<td class="col-lg-10" style="padding:0px;">
-							<div class="row">
-								<div class="form-group" >
-								{!! Form::text('subject', @$data['new'][0]['subject'], array('class'=>'form-control','placeholder'=>'Tiêu đề')) !!}
-								</br>
-								{!! Form::textarea('summary', @$data['new'][0]['summary'], array('class'=>'form-control','rows'=>'3','placeholder'=>'Nội dung tóm tắt')) !!}
-								</div>
-							</div>
-						</td>
-					</tr>
-				</table>
-				</br>
-				<div class="row">
-					{!! Form::textarea('content', @$data['new'][0]['content'], array('class'=>'form-control','rows'=>'4','cols'=>'1','id'=>'content')) !!}
-				</div>
-				</br>
-				<div class="row">
-					<div class="col-lg-4" style="padding:0px;">
-						<input type="submit" name="submit" class="btn btn-primary" value="@if (isset($data['new'])) Cập nhật bài viết @else Thêm bài viết @endif" />
-					</div>
-					<div class="col-lg-8" style="padding:0px;">
-					{!! Form::file('file', array('class'=>'form-control')) !!}
-					</div>
-					@if (isset($data['new']))
-						<input type="hidden" class="form-control" name="id" value="{{$data['new'][0]['new_id']}}" />
-					@endif
-				</div>
-				<hr style="border-top: 1px solid #CECECE"/>
-				<div class="row">
-					<table class="table table-bordered table_striped">
-						<tr>
-							<th style="text-align:center;width:60px;">STT</th>
-							<th style="width:340px;">Tiêu đề bài viết</th>
-							<th style="width:530px;">Nội dung tóm tắt</th>
-							<th style="width:128px;"></th>
-						</tr>
-						<?php $i=1; ?>
-						<?php foreach($data['list_new'] as $ln){ ?>
+				@if (isset($data['new']))
+					<input type="hidden" class="form-control" name="id" value="{{$data['new'][0]['new_id']}}" />
+				@endif
+				<div class="tab-content col-md-12" style="padding:0px;">
+					<div class="tab-pane active" id="tab1">
+						</br>
+						<table width="100%" class="form-table">
 							<tr>
-								<td style="text-align:center;"><?php echo $i++ ?></td>
-								<td><?php echo $ln['subject'] ?></td>
-								<td><?php echo $ln['summary'] ?></td>
-								<td align="center">
-									<a href="inew?action=edit&id={{ $ln['new_id'] }}" class="btn btn-default btn-xs" title="Sửa"><span class="glyphicon glyphicon-edit"></span> sửa</a>&nbsp;
-									<a href="inew?action=delete&id={{ $ln['new_id'] }}" onClick="return confirm('Bạn có chắc muốn xóa?');" title="Xóa" class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-trash"></span> xóa</a>
+								<td class="col-lg-2" style="padding:0px;">
+									<div class="form-group" >
+										<div class="upanh" id="upanh_1" data_id="1">
+											<a> 
+												<img class='col-lg-12' style="padding:6px;border:2px dashed #0087F7;height:130px;width:140" id="img_1" src="@if (isset($data['new'])){{asset('public/uploads/img/new_'.$data['new'][0]['new_id'].'.jpg')}}@else{{asset('public/img/add.png')}}@endif" alt="Chọn mẫu vật" />										
+											</a>									
+										</div>
+										<div id="btx_1">
+										</div>								
+										{!! Form::file('images', array('class'=>'form-control imgInp','style'=>'display:none;','id'=>'imgInp_1','data_id'=>'1')) !!}
+									</div>				
+								</td>
+								<td class="col-lg-10" style="padding:0px;">
+									<div class="row">
+										<div class="form-group" >
+										{!! Form::text('subject', @$data['new'][0]['subject'], array('class'=>'form-control','placeholder'=>'Tiêu đề')) !!}
+										</br>
+										{!! Form::textarea('summary', @$data['new'][0]['summary'], array('class'=>'form-control','rows'=>'3','placeholder'=>'Nội dung tóm tắt')) !!}
+										</div>
+									</div>
 								</td>
 							</tr>
-						<?php } ?>
-					</table>
-					<?php echo $data['list_new']->render(); ?>
+							<tr>
+								<td colspan="2">
+									{!! Form::textarea('content', @$data['new'][0]['content'], array('class'=>'form-control','rows'=>'4','cols'=>'1','id'=>'content')) !!}
+								</td>								
+							</tr>
+							<tr>
+								<td colspan="2">
+									<div class="col-lg-4" style="padding:0px;">
+										<a href="inew"><button type="button" class="btn btn-primary">Thêm bài viết mới</button></a>
+										<input type="submit" name="submit" class="btn btn-success" value="Gửi bài viết" />						
+									</div>
+									<div class="col-lg-8" style="padding:0px;">
+									{!! Form::file('file', array('class'=>'form-control')) !!}
+									</div>
+								</td>
+							</tr>
+						</table>
+					</div>
+					<div class="tab-pane" id="tab2">
+						<table class="table table-bordered table_striped">
+							<tr>
+								<th style="text-align:center;width:5%px;">STT</th>
+								<th style="width:70%;">Tiêu đề bài viết</th>
+								<th style="text-align:center;width:10%;">File</th>
+								<th style="width:15%;"></th>
+							</tr>
+							<?php $i=1; ?>
+							<?php foreach($data['list_new'] as $ln){ ?>
+								<tr>
+									<td style="text-align:center;"><?php echo $i++ ?></td>
+									<td><?php echo $ln['subject'] ?></td>
+									<td style="text-align:center;">
+										<?php if($ln['file']!=null){ ?>
+										<a href="/public/uploads/file/news/<?php echo $ln['file']?>/" target="_blank">tải file</a>
+										<?php } ?>
+									</td>
+									<td align="center">
+										<a href="inew?action=edit&id={{ $ln['new_id'] }}" class="btn btn-default btn-xs" title="Sửa"><span class="glyphicon glyphicon-edit"></span> sửa</a>&nbsp;
+										<a href="inew?action=delete&id={{ $ln['new_id'] }}" onClick="return confirm('Bạn có chắc muốn xóa?');" title="Xóa" class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-trash"></span> xóa</a>
+									</td>
+								</tr>
+							<?php } ?>
+						</table>
+						<?php echo $data['list_new']->render(); ?>
+					</div>
 				</div>
 			{!! Form::close() !!}
 		</div>
